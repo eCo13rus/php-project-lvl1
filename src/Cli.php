f752fe844f74c92aaf170e7da87342b6aa106326
Ilya Shirokov <asd.mat@mail.ru>
2023-03-15 17:11:26 +0300

    Update

---
+++ src/Cli.php
@@ -0,0 +1,17 @@
+<?php
+namespace BrainGames\Cli;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use function cli\line;
+use function cli\prompt;
+
+function greet()
+{
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
+
+
+?>
