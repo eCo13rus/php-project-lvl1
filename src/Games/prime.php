cc5671e0dc7c6250ef7f66555f1a2622156a9843
Ilya Shirokov <asd.mat@mail.ru>
2023-03-23 15:53:25 +0300

    fix

--- src/Games/prime.php
+++ src/Games/prime.php
@@ -2,7 +2,7 @@
 
 namespace Brain\Games;
 
-const PRIME_RULES = 'Answer "yes" if given number is prime. Otherwise answer "no"';
+const PRIME_RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const PRIME_RANGE = [0, 100];
 
 function primeGame()
