a1bb7af0964f69e67a987f410eab89923e381417
Ilya Shirokov <asd.mat@mail.ru>
2023-03-23 15:50:43 +0300

    fix

--- src/Games/prime.php
+++ src/Games/prime.php
@@ -3,7 +3,7 @@
 namespace Brain\Games;
 
 const PRIME_RULES = 'Answer "yes" if given number is prime. Otherwise answer "no"';
-const PRIME_RANGE = [0, 1000];
+const PRIME_RANGE = [0, 100];
 
 function primeGame()
 {
@@ -11,7 +11,7 @@ function primeGame()
 
     for ($i = 0; $i < ROUNDS_COUNT; $i++) {
         $question = rand(...PRIME_RANGE);
-        $rightAnswer = IsPrime($question) ? 'yes' : 'no';
+        $rightAnswer = isPrime($question) ? 'yes' : 'no';
         $rounds[] = [$question, $rightAnswer];
     }
 
